6f5326d2f1a46a9686b3140dd49e1e8941e0fca9
komushi <komushi@gmail.com>
2015-01-15 18:29:06 +0900

    first rest method done

--- server/nodeapp.js
+++ server/nodeapp.js
@@ -2,6 +2,7 @@ var application_root = __dirname;
 var express = require("express");
 var request = require('request');
 var path = require("path");
+var Q = require("q");
 var app = express();
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -17,6 +18,42 @@ var allowCrossDomain = function(req, res, next) {
     }
 };
 
+var getMongoUri = function() {
+
+  // return "mongodb://CloudFoundry_ids0og1r_hlaug1jk_6l8hvfn8:ji2HOfQbTjqXHo-pfEO6St0a_4sPZwp0@ds031601.mongolab.com:31601/CloudFoundry_ids0og1r_hlaug1jk";
+
+  var cfenv = require("cfenv");
+  var appEnv = cfenv.getAppEnv();
+  var services = appEnv.getServices();
+  console.log("services:" + JSON.stringify(services));
+  var myservice = appEnv.getService("gnavi_mongo");
+  var credentials = myservice.credentials;
+  console.log("credentials:" + credentials);
+  return credentials.uri;
+
+};
+
+var uri = getMongoUri();
+
+var getGnaviPrefs = function(db) {
+  var d = Q.defer();
+console.log("getGnaviPrefs");
+  db.prefecture.findOne(function(err, prefectureList) {
+    if(err || !prefectureList) 
+    {
+      d.reject(new Error(err));
+    }
+    else 
+    {
+      console.log("found");
+      d.resolve(prefectureList);
+
+    }
+  });
+
+  return d.promise;
+};
+
 // Config
 app.use(allowCrossDomain);
 app.use(express.static(path.join(application_root, "../client")));
@@ -28,20 +65,20 @@ app.get('/api', function (req, res) {
 
 
 app.get('/getGnaviPrefs', function (req, res) {
-  var url = "http://api.gnavi.co.jp/ver1/PrefSearchAPI/?keyid=3752190c2d640eb83d502e192085ccf9&format=json";
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+  
+  var mongojs = require('mongojs');
+  var db = mongojs(uri, ["prefecture"]);
 
+  getGnaviPrefs(db)
+    .then(function(prefectureList) {
       res.set('Content-Type', 'application/json');
-      res.send(body);
-    }
-    else
-    {
-      console.log(response.statusCode);
-      console.log(error);
+      res.send(prefectureList);
+    })
+    .done(function() {
+      console.log("close");
+      db.close();
+    });
 
-    }
-  })
 });
 
 app.get('/getGnaviAreas', function (req, res) {
